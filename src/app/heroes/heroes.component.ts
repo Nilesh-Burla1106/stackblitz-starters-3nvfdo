9fbd692362ece61c7cce8bf7e4f488f126dc2946
Nilesh-Burla1106 <141109890+Nilesh-Burla1106@users.noreply.github.com>
2023-08-13 21:16:03 +0530

    2 files modified, 3 added

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,16 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { HeroComponent } from './hero/hero.component';
 
 @Component({
   standalone:true,
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
+  imports:[HeroComponent,CommonModule]
 })
 export class HeroesComponent implements OnInit {
-
+  heroes:Hero[]=[];
   constructor() { }
 
   ngOnInit() {
+    this.heroes=HEROES;
   }
 
 }
